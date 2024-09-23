691e52ebbfd01a2c50ef5c78360724fc6a53a356
Prerna Gurung <114868160+prerna-gurung@users.noreply.github.com>
2024-09-23 07:56:53 +0000

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import "../styles/hero.css"
 function Hero() {
   return (
     <section className="hero-section">
-      <div className="container col-xxl-8 px-4 py-5">
+      <div className="container col-xxl-8 px-0 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6">
             <Lottie animationData={Myanimation} />
